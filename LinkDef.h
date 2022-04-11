6d069088dabd082142c23d284d8c89560f5d31d6
Greg Christian <greg.christian@smu.ca>
2022-04-11 11:30:51 -0300

    Added option to generate a ROOT-compatible libcatima.so

---
+++ LinkDef.h
@@ -0,0 +1,17 @@
+#ifdef __CLING__
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class catima::Projectile+;
+#pragma link C++ class catima::Material+;
+#pragma link C++ class catima::Layers+;
+#pragma link C++ class catima::Config+;
+#pragma link C++ class catima::Target+;
+#pragma link C++ class catima::Result+;
+#pragma link C++ class catima::MultiResult+;
+#pragma link C++ class catima::Phasespace+;
+
+
+#pragma link C++ defined_in "catima.h"+;
+#pragma link C++ defined_in "material_database.h"+;
+
+#endif
